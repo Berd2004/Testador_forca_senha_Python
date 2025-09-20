458448c1ca6d0cc414f7c1f98eb13e1e8a6ba816
Bernardo <costalopesbernardo@gmail.com>
2025-09-20 17:17:17 -0300

    feat: interface GUI criada

--- testador_forca_senha.py
+++ testador_forca_senha.py
@@ -1,16 +1,41 @@
+#importando a biblioteca e apelidando de "ctk" para facilitar a escrita das funções
+import customtkinter as ctk  
 
 
 
-
-def verificar_senha (senha: str):
+def verificar_senha():
+    senha = campo_senha.get()
     pontuacao = 0
     sugestoes = []
-    
-    
-print("/n ******************** TESTE A FORÇA DA SUA SENHA**************************")
-print("Digite sua senha:")
+    qtd_caracteres = len()
+
+    if qtd_caracteres > 8:
+        pontuacao += 1
+    else:
+        sugestoes.append("Use pelo menos 8 caracteres")
+        
+        
+#configurando a aparência
+ctk.set_appearance_mode('dark')
+
+# criação da janela principal
+app = ctk.CTk()
+app.title('Teste de Força de Senha') #É o título da tela (aparece o no topo)
+app.geometry('400x400') # Define o tamanho da tela do programa
 
 
-   
-    
+##Campos
+texto = ctk.CTkLabel(app, text='Teste de Força de Senha',font=("Arial", 22))
+texto.pack(pady='10')
 
+campo_senha = ctk.CTkEntry(app, placeholder_text='Teste sua senha aqui', show='')
+campo_senha.pack(pady='5')
+
+
+##botão
+
+botao = ctk.CTkButton(app, text='Testar', command='verificar_senha', width=70)
+botao.pack(pady='12')
+
+#Iniciar a aplicação
+app.mainloop()
